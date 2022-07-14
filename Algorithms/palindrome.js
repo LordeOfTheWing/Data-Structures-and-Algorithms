73134b7ce04c4dd73854b3687175cda582df1936
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-14 11:37:12 +0300

    Implement palindrome checker algorithm

---
+++ Algorithms/palindrome.js
@@ -0,0 +1,17 @@
+//Given a string , return true or false depending if the string is
+// a palindrome. Palindromes are words that read the same backwards
+// and forwards.Make sure it is case insensitive.
+//--Examples
+// palindrome("Madam") === true
+// palindrome('love') === false
+
+function palindrome(str) {
+  const reversedString = str.split("").reverse().join("");
+  if (str !== reversedString) {
+    return false;
+  }
+  return true;
+}
+
+console.log(palindrome("madam"));
+console.log(palindrome("racecar"));
