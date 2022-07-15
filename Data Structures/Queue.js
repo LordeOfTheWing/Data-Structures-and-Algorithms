8b3094a9e4582735c539a61abd9dc3153c2af886
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-15 12:54:25 +0300

    Implement a more optimised Queue data structure

--- Data Structures/Queue.js
+++ Data Structures/Queue.js
@@ -6,21 +6,26 @@ class Queue {
   enqueue(element) {
     this.items.push(element);
   }
+
   dequeue() {
     return this.items.shift();
   }
+
   peek() {
     if (!this.isEmpty()) {
       return this.items[0];
     }
     return null;
   }
+
   isEmpty() {
     return this.items.length === 0;
   }
+
   size() {
     return this.items.length;
   }
+
   print() {
     return this.items.toString();
   }
@@ -31,7 +36,56 @@ console.log(queue.enqueue(1));
 console.log(queue.enqueue(2));
 console.log(queue.enqueue(3));
 console.log(queue.dequeue());
-console.log(queue.print(1));
+console.log(queue.print());
 console.log(queue.isEmpty());
 console.log(queue.peek());
 console.log(queue.size());
+
+//Optimising the queue
+class Queue2 {
+  constructor() {
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    return this.items;
+  }
+}
+
+const queue2 = new Queue2();
+console.log(queue2.print());
+console.log(queue2.enqueue(1));
+console.log(queue2.enqueue(2));
+console.log(queue2.enqueue(3));
+console.log(queue2.dequeue());
+console.log(queue2);
+console.log(queue2.print());
+console.log(queue2.peek());
+console.log(queue2.size());
+console.log(queue2.isEmpty());
