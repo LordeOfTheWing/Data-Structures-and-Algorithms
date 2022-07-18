3ea88d92aa1dc73471f277d9525ccf26f33182b8
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-18 23:00:08 +0300

    Create a Node class and Linked list class

---
+++ Data Structures/LinkedList.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const linkedList = new LinkedList();
+console.log("List is empty? ", linkedList.isEmpty());
+console.log("List size:", linkedList.getSize());
