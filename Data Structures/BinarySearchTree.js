0b36a65674c4edd06aee635bd8459b6f4707e389
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-28 00:45:49 +0300

    Implement a node and binary search tree class which has an isEmpty method to check if it has any nodes

---
+++ Data Structures/BinarySearchTree.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+}
+
+const BST = new BinarySearchTree();
+console.log("Is tree empty?", BST.isEmpty());
