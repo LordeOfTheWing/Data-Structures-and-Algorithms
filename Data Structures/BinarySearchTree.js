16dfeb82ea0685e92517c14518e6b5ad96fce775
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-28 01:02:19 +0300

    Implement an insert method that adds node(s) to the binary search tree

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -14,7 +14,35 @@ class BinarySearchTree {
   isEmpty() {
     return this.root === null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(root, newNode) {
+    //Check whether the newNode to be inserted value is less than the root node value.
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
 }
 
 const BST = new BinarySearchTree();
+BST.insert(10);
+BST.insert(5);
+BST.insert(15);
 console.log("Is tree empty?", BST.isEmpty());
