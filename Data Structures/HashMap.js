c5fc6f37f58cf3515d5f957a01cf3036de31b542
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-28 00:26:53 +0300

    Refactor the set,get and remove methods in the hashmap to fix collisions

--- Data Structures/HashMap.js
+++ Data Structures/HashMap.js
@@ -14,17 +14,46 @@ class HashTable {
 
   set(key, value) {
     const index = this.hash(key);
-    this.table[index] = value;
+    // this.table[index] = value;
+    const bucket = this.table[index];
+
+    if (!bucket) {
+      this.table[index] = [[key, value]];
+    } else {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        sameKeyItem[1] = value;
+      } else {
+        bucket.push([key, value]);
+      }
+    }
   }
 
   get(key) {
     const index = this.hash(key);
-    return this.table[index];
+    // return this.table[index];
+
+    const bucket = this.table[index];
+
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        return sameKeyItem[1];
+      }
+    }
+    return undefined;
   }
 
   remove(key) {
     const index = this.hash(key);
-    this.table[index] = undefined;
+    // this.table[index] = undefined;
+    const bucket = this.table[index];
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        bucket.splice(bucket.indexOf(sameKeyItem), 1);
+      }
+    }
   }
 
   display() {
@@ -38,5 +67,7 @@ class HashTable {
 
 const table = new HashTable(10);
 table.set("Key", "Hello World");
+table.set("eyK", "Hello there");
 console.log(table.get("Key"));
+table.remove("eyK");
 table.display();
