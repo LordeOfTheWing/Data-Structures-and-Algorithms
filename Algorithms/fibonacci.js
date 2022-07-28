26e3535c0154b4cba32d6564f5064965272f6c73
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-28 12:52:53 +0300

    Implement an algorithm that returns the elements in a fibonnaci sequence

---
+++ Algorithms/fibonacci.js
@@ -0,0 +1,17 @@
+//Given a number 'n', find the first 'n' elements of the fibonacci sequence
+//Examples:
+// fibonacci(2) returns [0,1]
+// fibonacci(3) returns [0,1,1]
+// fibonacci(7) returns [0,1,1,2,3,5,8]
+
+function fibonacci(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
