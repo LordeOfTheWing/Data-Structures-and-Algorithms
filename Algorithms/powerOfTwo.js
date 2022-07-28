9912243b2509eb1c59b3c32e74711b985c2a336e
Clinton Mainya <klyntonmainya@gmail.com>
2022-07-28 21:21:23 +0300

    Implement an algorithm that checks whether a given number is a power of two

---
+++ Algorithms/powerOfTwo.js
@@ -0,0 +1,24 @@
+//Given a positive integer 'n', determine if the number is a power of 2 or not
+//Example:
+//isPowerOfTwo(1) returns true
+//isPowerOfTwo(2) returns true
+//isPowerOfTwo(5) returns false
+
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n = n / 2;
+  }
+
+  return true;
+}
+
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(5));
